c83c97ebc99cc9c2cc9eed1ba9da22cbed40601b
Kyle Corbitt <kyle@corbt.com>
2022-04-05 10:56:35 +0200

    can initialize the program

--- programs/realitycoin_consensus/src/lib.rs
+++ programs/realitycoin_consensus/src/lib.rs
@@ -1,15 +1,67 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3nFoQdq56rXxQgLGQidrBa2Qfqj75c6NhmDCvdJqMEN9");
 
 #[program]
 pub mod realitycoin_consensus {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, min_stake: u64) -> Result<()> {
+        ctx.accounts.program_data.min_stake = min_stake;
+        Ok(())
+    }
+
+    pub fn add_validator(ctx: Context<AddValidator>, tokens_to_stake: u64) -> Result<()> {
+        // ctx.accounts.staked_validator.validator = ctx.accounts.validator.key();
+
+        // TODO: actually transfer the tokens!
+        // ctx.accounts.staked_validator.stake = tokens_to_stake;
+
         Ok(())
     }
 }
 
+#[account]
+pub struct StakedValidator {
+    pub validator: Pubkey,
+    pub stake: u64,
+}
+
+#[account]
+pub struct ProgramData {
+    pub min_stake: u64,
+    pub total_staked: u64,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+        init,
+        payer = owner,
+        seeds = [b"program-data", owner.key().as_ref()],
+        bump,
+        space = 8 + 8 + 8
+    )]
+    pub program_data: Account<'info, ProgramData>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddValidator<'info> {
+    #[account(mut)]
+    pub validator: Signer<'info>,
+
+    #[account(
+        init,
+        payer = validator,
+        seeds = [b"staked-validator", validator.key().as_ref()],
+        bump,
+        space = 8 + 32 + 8
+    )]
+    pub staked_validator: Account<'info, StakedValidator>,
+    pub system_program: Program<'info, System>,
+}
--- tests/realitycoin_consensus.ts
+++ tests/realitycoin_consensus.ts
@@ -1,16 +1,57 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { expect } from "chai";
 import { RealitycoinConsensus } from "../target/types/realitycoin_consensus";
 
-describe("realitycoin_consensus", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+const MIN_STAKE = new anchor.BN(5000);
+const program = anchor.workspace.RealitycoinConsensus as Program<RealitycoinConsensus>;
+const owner = program.provider.wallet;
 
-  const program = anchor.workspace.RealitycoinConsensus as Program<RealitycoinConsensus>;
+const airdrop = async (account: anchor.web3.Keypair) =>
+  await program.provider.connection.confirmTransaction(
+    await program.provider.connection.requestAirdrop(account.publicKey, 1000000000),
+    "processed"
+  );
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+anchor.setProvider(anchor.Provider.env());
+
+describe("realitycoin_consensus", async () => {
+  const [programDataPDA, _] = await PublicKey.findProgramAddress(
+    [anchor.utils.bytes.utf8.encode("program-data"), owner.publicKey.toBuffer()],
+    program.programId
+  );
+
+  it("Initializes correctly", async () => {
+    await program.rpc.initialize(MIN_STAKE, {
+      accounts: {
+        programData: programDataPDA,
+        owner: owner.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+    });
+
+    const programData = await program.account.programData.fetch(programDataPDA);
+    expect(programData.minStake.toString()).equal(MIN_STAKE.toString());
+  });
+
+  it("Lets users stake", async () => {
+    const validator1 = anchor.web3.Keypair.generate();
+    await airdrop(validator1);
+
+    const [stakedValidatorPDA, _] = await PublicKey.findProgramAddress(
+      [anchor.utils.bytes.utf8.encode("staked-validator"), validator1.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const stake: anchor.BN = new anchor.BN(5000);
+    await program.rpc.addValidator(stake, {
+      accounts: {
+        stakedValidator: stakedValidatorPDA,
+        validator: validator1.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [validator1],
+    });
   });
 });
