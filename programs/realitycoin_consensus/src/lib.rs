f9a8dfd1787ceda29bbb2ae57100925ccbc67797
Kyle Corbitt <kyle@corbt.com>
2022-04-05 16:14:49 +0200

    can propose blocks

--- programs/realitycoin_consensus/src/lib.rs
+++ programs/realitycoin_consensus/src/lib.rs
@@ -1,6 +1,10 @@
 use anchor_lang::prelude::*;
+use std::mem;
 
 declare_id!("3nFoQdq56rXxQgLGQidrBa2Qfqj75c6NhmDCvdJqMEN9");
+
+// Give validators a maximum of 4 hours to process a block
+const MAX_VOTING_TIME_IN_SECONDS: u32 = 60 * 60 * 4;
 
 #[program]
 pub mod realitycoin_consensus {
@@ -12,13 +16,42 @@ pub mod realitycoin_consensus {
     }
 
     pub fn add_validator(ctx: Context<AddValidator>, tokens_to_stake: u64) -> Result<()> {
-        // ctx.accounts.staked_validator.validator = ctx.accounts.validator.key();
+        ctx.accounts.staked_validator.validator = ctx.accounts.validator.key();
 
-        // TODO: actually transfer the tokens!
-        // ctx.accounts.staked_validator.stake = tokens_to_stake;
+        // TODO: This is a placeholder. Need to actually transfer the tokens!
+        ctx.accounts.staked_validator.stake = tokens_to_stake;
+        ctx.accounts.program_data.total_staked += tokens_to_stake;
 
         Ok(())
     }
+
+    pub fn start_block_vote(
+        ctx: Context<StartBlockVote>,
+
+        // TODO: I'm using the `Pubkey` type here because it's a convenient
+        // existing 32-byte container. Need to figure out how to implement a
+        // custom BlockHash type and implement serialization/deserialization.
+        block_hash: Pubkey,
+        miner: Pubkey,
+    ) -> Result<()> {
+        ctx.accounts.block_vote.block_hash = block_hash;
+        ctx.accounts.block_vote.miner = miner;
+        ctx.accounts.block_vote.validator = ctx.accounts.validator.key();
+
+        let now = Clock::get().unwrap().unix_timestamp;
+        ctx.accounts.block_vote.expires_at = (now + MAX_VOTING_TIME_IN_SECONDS as i64) as u64;
+
+        ctx.accounts.program_data.active_votes.push(block_hash);
+
+        Ok(())
+    }
+}
+
+#[account]
+pub struct ProgramData {
+    pub min_stake: u64,
+    pub total_staked: u64,
+    pub active_votes: Vec<Pubkey>,
 }
 
 #[account]
@@ -28,9 +61,13 @@ pub struct StakedValidator {
 }
 
 #[account]
-pub struct ProgramData {
-    pub min_stake: u64,
-    pub total_staked: u64,
+pub struct BlockVote {
+    pub block_hash: Pubkey,
+    pub miner: Pubkey,
+    pub validator: Pubkey,
+    pub approve_votes: u32,
+    pub reject_votes: u32,
+    pub expires_at: u64,
 }
 
 #[derive(Accounts)]
@@ -41,9 +78,9 @@ pub struct Initialize<'info> {
     #[account(
         init,
         payer = owner,
-        seeds = [b"program-data", owner.key().as_ref()],
+        seeds = [b"program-data"],
         bump,
-        space = 8 + 8 + 8
+        space = 8 + 8 + 8 + 32 * 100
     )]
     pub program_data: Account<'info, ProgramData>,
 
@@ -60,8 +97,30 @@ pub struct AddValidator<'info> {
         payer = validator,
         seeds = [b"staked-validator", validator.key().as_ref()],
         bump,
-        space = 8 + 32 + 8
+        space = 8 + mem::size_of::<StakedValidator>()
     )]
     pub staked_validator: Account<'info, StakedValidator>,
+    #[account(mut)]
+    pub program_data: Account<'info, ProgramData>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(block_hash: Pubkey, miner: Pubkey)]
+pub struct StartBlockVote<'info> {
+    #[account(
+        init,
+        payer=validator,
+        // seeds=[b"block-vote"],
+        seeds=[b"block-vote", block_hash.key().as_ref()],
+        bump,
+        space=8+mem::size_of::<BlockVote>()
+    )]
+    pub block_vote: Account<'info, BlockVote>,
+
+    #[account(mut)]
+    pub validator: Signer<'info>,
+    #[account(mut)]
+    pub program_data: Account<'info, ProgramData>,
     pub system_program: Program<'info, System>,
 }
--- tests/realitycoin_consensus.ts
+++ tests/realitycoin_consensus.ts
@@ -1,28 +1,46 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { PublicKey, SystemProgram } from "@solana/web3.js";
-import { expect } from "chai";
+import chai, { expect } from "chai";
 import { RealitycoinConsensus } from "../target/types/realitycoin_consensus";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
 
 const MIN_STAKE = new anchor.BN(5000);
-const program = anchor.workspace.RealitycoinConsensus as Program<RealitycoinConsensus>;
-const owner = program.provider.wallet;
-
-const airdrop = async (account: anchor.web3.Keypair) =>
-  await program.provider.connection.confirmTransaction(
-    await program.provider.connection.requestAirdrop(account.publicKey, 1000000000),
-    "processed"
-  );
-
 anchor.setProvider(anchor.Provider.env());
+const program = anchor.workspace.RealitycoinConsensus as Program<RealitycoinConsensus>;
 
 describe("realitycoin_consensus", async () => {
-  const [programDataPDA, _] = await PublicKey.findProgramAddress(
-    [anchor.utils.bytes.utf8.encode("program-data"), owner.publicKey.toBuffer()],
-    program.programId
-  );
+  const owner = program.provider.wallet;
+
+  const getPDA = async (seeds: (string | PublicKey | Buffer)[]) => {
+    const encodedSeeds = seeds.map((seed) =>
+      seed instanceof Buffer
+        ? seed
+        : seed instanceof PublicKey
+        ? seed.toBuffer()
+        : anchor.utils.bytes.utf8.encode(seed)
+    );
+
+    const [address, _] = await PublicKey.findProgramAddress(encodedSeeds, program.programId);
+
+    return address;
+  };
+
+  const getProgramData = async (pda: Awaited<ReturnType<typeof getPDA>>) =>
+    program.account.programData.fetch(await getPDA(["program-data"]));
+
+  const airdrop = async (account: anchor.web3.Keypair) =>
+    await program.provider.connection.confirmTransaction(
+      await program.provider.connection.requestAirdrop(account.publicKey, 1000000000),
+      "processed"
+    );
+
+  const validator1 = anchor.web3.Keypair.generate();
 
   it("Initializes correctly", async () => {
+    const programDataPDA = await getPDA(["program-data"]);
     await program.rpc.initialize(MIN_STAKE, {
       accounts: {
         programData: programDataPDA,
@@ -31,27 +49,75 @@ describe("realitycoin_consensus", async () => {
       },
     });
 
-    const programData = await program.account.programData.fetch(programDataPDA);
+    const programData = await getProgramData(programDataPDA);
     expect(programData.minStake.toString()).equal(MIN_STAKE.toString());
+    expect(programData.totalStaked.toString()).equal("0");
   });
 
-  it("Lets users stake", async () => {
-    const validator1 = anchor.web3.Keypair.generate();
+  it("Can't be initialized twice", async () => {
+    const programDataPDA = await getPDA(["program-data"]);
+
+    await expect(
+      program.rpc.initialize(MIN_STAKE, {
+        accounts: {
+          programData: programDataPDA,
+          owner: owner.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+      })
+    ).to.eventually.be.rejectedWith("already been processed");
+  });
+
+  it("Lets users sign up as validators", async () => {
+    const programData = await getPDA(["program-data"]);
+
     await airdrop(validator1);
 
-    const [stakedValidatorPDA, _] = await PublicKey.findProgramAddress(
-      [anchor.utils.bytes.utf8.encode("staked-validator"), validator1.publicKey.toBuffer()],
-      program.programId
-    );
+    const validator1PDA = await getPDA(["staked-validator", validator1.publicKey]);
 
     const stake: anchor.BN = new anchor.BN(5000);
     await program.rpc.addValidator(stake, {
       accounts: {
-        stakedValidator: stakedValidatorPDA,
+        stakedValidator: validator1PDA,
         validator: validator1.publicKey,
         systemProgram: SystemProgram.programId,
+        programData,
       },
       signers: [validator1],
     });
+
+    expect((await getProgramData(programData)).totalStaked.toString()).to.equal(
+      new anchor.BN(5000).toString()
+    );
+  });
+
+  it("Starts a block vote", async () => {
+    const miner = anchor.web3.Keypair.generate();
+    const programDataPDA = await getPDA(["program-data"]);
+    const blockHash = anchor.web3.Keypair.generate().publicKey;
+
+    const blockVotePDA = await getPDA(["block-vote", blockHash]);
+
+    await program.rpc.startBlockVote(blockHash, miner.publicKey, {
+      accounts: {
+        blockVote: blockVotePDA,
+        programData: programDataPDA,
+        systemProgram: SystemProgram.programId,
+        validator: validator1.publicKey,
+      },
+      signers: [validator1],
+    });
+
+    const programData = await getProgramData(programDataPDA);
+    expect(programData.activeVotes[0].toString()).to.equal(blockHash.toString());
+
+    const blockVote = await program.account.blockVote.fetch(blockVotePDA);
+
+    expect(blockVote.miner.toString()).to.equal(miner.publicKey.toString());
+    expect(blockVote.blockHash.toString()).to.equal(blockHash.toString());
+    expect(blockVote.approveVotes.toString()).to.equal("0");
+    expect(blockVote.rejectVotes.toString()).to.equal("0");
+    expect(blockVote.expiresAt.toNumber()).greaterThan(new Date().getTime() / 1000);
+    expect(blockVote.expiresAt.toNumber()).lessThan(new Date().getTime() / 1000 + 60 * 60 * 5);
   });
 });
